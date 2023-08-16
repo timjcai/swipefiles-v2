9dc94798e879ddae1476ce7037ee41a5427d1f93
timjcai <tim.j.cai@gmail.com>
2023-08-16 21:41:13 +1000

    feat: added success screen after sign up

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -65,209 +65,243 @@ export const Register = () => {
 
     const signUp = (e) => {
         e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                console.log(userCredential);
-                updateProfile(auth.currentUser, {
-                    displayName: user,
-                })
-                    .then(() => {
-                        console.log("added username to user account");
-                        console.log(auth.currentUser);
+        const validation1 = USERNAME_REGEX.test(user);
+        const validation2 = PWD_REGEX.test(password);
+
+        if (!validation1 || !validation2) {
+            setErrMsg("Invalid Entry");
+            return;
+        } else {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    console.log(userCredential);
+                    updateProfile(auth.currentUser, {
+                        displayName: user,
                     })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+                        .then(() => {
+                            console.log("added username to user account");
+                            console.log(auth.currentUser);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            setSuccess(true);
+        }
     };
 
     return (
-        <SectionRegistration>
-            <p
-                ref={errRef}
-                className={errMsg ? "errmsg" : "offscreen"}
-                aria-live="assertive"
-            >
-                {errMsg}
-            </p>
-            <h1>Create an account</h1>
-            <StyledRegisterForm onSubmit={signUp}>
-                <label htmlFor="username">
-                    Username:
-                    <span className={validName ? "valid" : "hide"}>
-                        <Icon label={"Valid"} />
-                    </span>
-                    <span className={validName || !user ? "hide" : "invalid"}>
-                        <Icon label={"Invalid"} />
-                    </span>
-                </label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUser(e.target.value)}
-                    required
-                    aria-invalid={validName ? "false" : "true"}
-                    aria-describedby="uidnote"
-                    onFocus={() => setUserFocus(true)}
-                    onBlur={() => setUserFocus(false)}
-                />
-                <p
-                    id="uidnote"
-                    className={
-                        userFocus && user && !validName
-                            ? "instructions"
-                            : "offscreen"
-                    }
-                >
-                    <Icon label={"Info"} />
-                    4 to 24 characters.
-                    <br />
-                    Must begin with a letter. <br />
-                    Letters, numbers, underscores, hypens allowed.
-                </p>
+        <>
+            {success ? (
+                <SectionRegistration>
+                    <h1>Success!</h1>
+                    <Link to="/swipes">Start Swiping</Link>
+                </SectionRegistration>
+            ) : (
+                <SectionRegistration>
+                    <p
+                        ref={errRef}
+                        className={errMsg ? "errmsg" : "offscreen"}
+                        aria-live="assertive"
+                    >
+                        {errMsg}
+                    </p>
+                    <h1>Create an account</h1>
+                    <StyledRegisterForm onSubmit={signUp}>
+                        <label htmlFor="username">
+                            Username:
+                            <span className={validName ? "valid" : "hide"}>
+                                <Icon label={"Valid"} />
+                            </span>
+                            <span
+                                className={
+                                    validName || !user ? "hide" : "invalid"
+                                }
+                            >
+                                <Icon label={"Invalid"} />
+                            </span>
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p
+                            id="uidnote"
+                            className={
+                                userFocus && user && !validName
+                                    ? "instructions"
+                                    : "offscreen"
+                            }
+                        >
+                            <Icon label={"Info"} />
+                            4 to 24 characters.
+                            <br />
+                            Must begin with a letter. <br />
+                            Letters, numbers, underscores, hypens allowed.
+                        </p>
 
-                <label htmlFor="email">
-                    Email:
-                    <span className={validEmail ? "valid" : "hide"}>
-                        <Icon label={"Valid"} />
-                    </span>
-                    <span className={validEmail || !email ? "hide" : "invalid"}>
-                        <Icon label={"Invalid"} />
-                    </span>
-                </label>
-                <input
-                    type="text"
-                    id="email"
-                    autoComplete="off"
-                    placeholder="Enter your email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    aria-invalid={validEmail ? "false" : "true"}
-                    aria-describedby="emailnote"
-                    onFocus={() => setEmailFocus(true)}
-                    onBlur={() => setEmailFocus(false)}
-                />
-                <p
-                    id="emailnote"
-                    className={
-                        emailFocus && email && !validEmail
-                            ? "instructions"
-                            : "offscreen"
-                    }
-                >
-                    <Icon label={"Info"} />
-                    {/* 4 to 24 characters.
-                    <br /> */}
-                    Must begin with a letter. <br />
-                    Letters, numbers, underscores, hypens allowed.
-                </p>
+                        <label htmlFor="email">
+                            Email:
+                            <span className={validEmail ? "valid" : "hide"}>
+                                <Icon label={"Valid"} />
+                            </span>
+                            <span
+                                className={
+                                    validEmail || !email ? "hide" : "invalid"
+                                }
+                            >
+                                <Icon label={"Invalid"} />
+                            </span>
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            autoComplete="off"
+                            placeholder="Enter your email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            aria-invalid={validEmail ? "false" : "true"}
+                            aria-describedby="emailnote"
+                            onFocus={() => setEmailFocus(true)}
+                            onBlur={() => setEmailFocus(false)}
+                        />
+                        <p
+                            id="emailnote"
+                            className={
+                                emailFocus && email && !validEmail
+                                    ? "instructions"
+                                    : "offscreen"
+                            }
+                        >
+                            <Icon label={"Info"} />
+                            {/* 4 to 24 characters.
+        <br /> */}
+                            Must begin with a letter. <br />
+                            Letters, numbers, underscores, hypens allowed.
+                        </p>
 
-                <label htmlFor="password">
-                    Password:
-                    <Icon
-                        label={"Valid"}
-                        className={validPassword ? "valid" : "hide"}
-                    />
-                    <Icon
-                        label={"Invalid"}
-                        className={
-                            validPassword || !password ? "hide" : "invalid"
-                        }
-                    />
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                    aria-invalid={validPassword ? "false" : "true"}
-                    aria-describedby="passwordnote"
-                    onFocus={() => setPasswordFocus(true)}
-                    onBlur={() => setPasswordFocus(false)}
-                />
-                <p
-                    id="passwordnote"
-                    className={
-                        passwordFocus && !validPassword
-                            ? "instructions"
-                            : "offscreen"
-                    }
-                >
-                    <Icon label={"Info"} />
-                    8 to 24 characters.
-                    <br />
-                    Must include uppercase and lowercase letters, a number and a
-                    special character.
-                    <br />
-                    Allowed special characters:{" "}
-                    <span aria-label="exclamation mark">!</span>{" "}
-                    <span aria-label="at symbol">@</span>{" "}
-                    <span aria-label="hashtag">#</span>{" "}
-                    <span aria-label="dollar sign">$</span>{" "}
-                    <span aria-label="percent">%</span>
-                </p>
+                        <label htmlFor="password">
+                            Password:
+                            <Icon
+                                label={"Valid"}
+                                className={validPassword ? "valid" : "hide"}
+                            />
+                            <Icon
+                                label={"Invalid"}
+                                className={
+                                    validPassword || !password
+                                        ? "hide"
+                                        : "invalid"
+                                }
+                            />
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            placeholder="Enter your password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            required
+                            aria-invalid={validPassword ? "false" : "true"}
+                            aria-describedby="passwordnote"
+                            onFocus={() => setPasswordFocus(true)}
+                            onBlur={() => setPasswordFocus(false)}
+                        />
+                        <p
+                            id="passwordnote"
+                            className={
+                                passwordFocus && !validPassword
+                                    ? "instructions"
+                                    : "offscreen"
+                            }
+                        >
+                            <Icon label={"Info"} />
+                            8 to 24 characters.
+                            <br />
+                            Must include uppercase and lowercase letters, a
+                            number and a special character.
+                            <br />
+                            Allowed special characters:{" "}
+                            <span aria-label="exclamation mark">!</span>{" "}
+                            <span aria-label="at symbol">@</span>{" "}
+                            <span aria-label="hashtag">#</span>{" "}
+                            <span aria-label="dollar sign">$</span>{" "}
+                            <span aria-label="percent">%</span>
+                        </p>
 
-                <label htmlFor="confirm_password">
-                    Confirm Password:
-                    <Icon
-                        label={"Valid"}
-                        className={
-                            validMatch && matchPassword ? "valid" : "hide"
-                        }
-                    />
-                    <Icon
-                        label={"Invalid"}
-                        className={
-                            validMatch || !matchPassword ? "hide" : "invalid"
-                        }
-                    />
-                </label>
-                <input
-                    type="password"
-                    id="confirm_password"
-                    placeholder="Confirm your password"
-                    onChange={(e) => setMatchPassword(e.target.value)}
-                    value={matchPassword}
-                    required
-                    aria-invalid={validMatch ? "false" : "true"}
-                    aria-describedby="confirmnote"
-                    onFocus={() => setMatchFocus(true)}
-                    onBlur={() => setMatchFocus(false)}
-                />
-                <p
-                    id="confirmnote"
-                    className={
-                        matchFocus && !validMatch ? "instructions" : "offscreen"
-                    }
-                >
-                    <Icon label={"Info"} />
-                    Must match the first password input field.
-                </p>
+                        <label htmlFor="confirm_password">
+                            Confirm Password:
+                            <Icon
+                                label={"Valid"}
+                                className={
+                                    validMatch && matchPassword
+                                        ? "valid"
+                                        : "hide"
+                                }
+                            />
+                            <Icon
+                                label={"Invalid"}
+                                className={
+                                    validMatch || !matchPassword
+                                        ? "hide"
+                                        : "invalid"
+                                }
+                            />
+                        </label>
+                        <input
+                            type="password"
+                            id="confirm_password"
+                            placeholder="Confirm your password"
+                            onChange={(e) => setMatchPassword(e.target.value)}
+                            value={matchPassword}
+                            required
+                            aria-invalid={validMatch ? "false" : "true"}
+                            aria-describedby="confirmnote"
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+                        />
+                        <p
+                            id="confirmnote"
+                            className={
+                                matchFocus && !validMatch
+                                    ? "instructions"
+                                    : "offscreen"
+                            }
+                        >
+                            <Icon label={"Info"} />
+                            Must match the first password input field.
+                        </p>
 
-                <button
-                    disabled={
-                        !validName || !validPassword || !validMatch
-                            ? true
-                            : false
-                    }
-                    type="submit"
-                >
-                    Create account
-                </button>
-                <p>
-                    Already registered?
-                    <br />
-                    <Link to="/sign-in">Sign in</Link>
-                </p>
-            </StyledRegisterForm>
-        </SectionRegistration>
+                        <button
+                            disabled={
+                                !validName || !validPassword || !validMatch
+                                    ? true
+                                    : false
+                            }
+                            type="submit"
+                        >
+                            Create account
+                        </button>
+                        <p>
+                            Already registered?
+                            <br />
+                            <Link to="/sign-in">Sign in</Link>
+                        </p>
+                    </StyledRegisterForm>
+                </SectionRegistration>
+            )}
+        </>
     );
 };
 
