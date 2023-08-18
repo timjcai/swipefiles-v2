a18136957c8e2600f4cf9c4997c6d5db1ed62afa
timjcai <tim.j.cai@gmail.com>
2023-08-18 17:08:17 +1000

    Feat: Completed Sidebar functionality
    Now able to toggle between pages within App only
    (1) we have a 'selected' box to highlight which tab is currently open

--- src/components/navbar/Sidebar.tsx
+++ src/components/navbar/Sidebar.tsx
@@ -1,52 +1,65 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Icon } from "../common/Icon";
+import { NavigationTypes } from "../../types";
 
 export const Sidebar = () => {
+    const [selected, setSelected] = useState(null);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setSelected(e.target.id);
+    };
+
     return (
         <SidebarWrapper>
             <StyledDiv padding={"3rem 1.5rem"} justify={"space-between"}>
-                {" "}
                 <div>
                     <BetweenRow>
                         <div>Logo</div>
                     </BetweenRow>
                     <InlineColumn>
-                        <Link to="/dashboard">
-                            <Icon label="Home" />
-                            <span> </span>
-                            Home
+                        <Link to="/dashboard" onClick={handleClick}>
+                            <SidenavButton
+                                label="Home"
+                                selected={selected}
+                                onClick={handleClick}
+                            />
                         </Link>
                         <Link to="/swipes">
-                            {" "}
-                            <Icon label="Swipes" />
-                            <span> </span>
-                            Swipes
+                            <SidenavButton
+                                label="Swipes"
+                                selected={selected}
+                                onClick={handleClick}
+                            />
                         </Link>
                         <Link to="/create">
-                            {" "}
-                            <Icon label="Create" />
-                            <span> </span>
-                            Create
+                            <SidenavButton
+                                label="Create"
+                                selected={selected}
+                                onClick={handleClick}
+                            />
                         </Link>
                         <Link to="/boards">
-                            {" "}
-                            <Icon label="Boards" />
-                            <span> </span>
-                            Boards
+                            <SidenavButton
+                                label="Boards"
+                                selected={selected}
+                                onClick={handleClick}
+                            />
                         </Link>
                     </InlineColumn>
                 </div>
                 <div>
                     <div>Picture</div>
+                    <p>{selected}</p>
                     <div>
-                        {" "}
-                        <Link to="/settings">
-                            {" "}
-                            <Icon label="Settings" />
-                            <span> </span>
-                            Settings
+                        <Link to="/settings" onClick={handleClick}>
+                            <SidenavButton
+                                label="Settings"
+                                selected={selected}
+                                onClick={handleClick}
+                            />
                         </Link>
                     </div>
                 </div>
@@ -103,3 +116,56 @@ const InlineColumn = styled.div`
     flex-direction: column;
     gap: 15px;
 `;
+
+const IconSpacer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    cursor: pointer;
+    justify-content: center;
+`;
+
+const Selected = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    cursor: pointer;
+    background: #1164a3;
+    border-radius: 6px;
+    height: 28px;
+    color: #ffffff;
+    width: 100%;
+`;
+
+type SidenavButtonProps = {
+    label: NavigationTypes;
+    selected: NavigationTypes | null;
+    onClick?: (e: Event) => void;
+};
+
+export const SidenavButton: FC<SidenavButtonProps> = ({
+    label,
+    selected,
+    onClick,
+}) => {
+    const isSelected = selected === label;
+
+    return (
+        <>
+            {isSelected ? (
+                <IconSpacer id={label} onClick={onClick}>
+                    <Selected>
+                        <Icon label={label} />
+                        {label}
+                    </Selected>
+                </IconSpacer>
+            ) : (
+                <IconSpacer id={label} onClick={onClick}>
+                    <Icon label={label} />
+                    {label}
+                </IconSpacer>
+            )}
+        </>
+    );
+};
