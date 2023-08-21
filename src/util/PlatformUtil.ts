4e4606414dab4216b3d154091f7d3ba6d0dc8c7f
timjcai <tim.j.cai@gmail.com>
2023-08-21 20:24:57 +1000

    adding extract YT title - test: does it add value to our bookmark saver?

--- src/util/PlatformUtil.ts
+++ src/util/PlatformUtil.ts
@@ -42,3 +42,24 @@ export const extractAndFormatDomain = (url) => {
         return capitalizeFirstLetter(domain)
     }
 }
+
+export const extractYTTitle = (url) => {
+    fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(html => {
+    const dom = new JSDOM(html);
+    const document = dom.window.document.head;
+    // Example: Extracting all links from the page
+    const links = document.querySelector('title').innerHTML;
+    console.log(links)
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
+}
